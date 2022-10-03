b168929168bfeee57f7ab0d5899c47c163bdf205
ogkama <79057522+ogkama@users.noreply.github.com>
2022-10-03 22:07:52 +0300

    Please7

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -6,14 +6,14 @@ export default class ProjectCard extends Component {
     render() {
         return (
                 
-                    <Card xs sm={8} style={{ width: '25rem' }}>
+                    <Card xs sm={8} className = "plholder" style={{ width: '25rem' }}>
                         <Card.Img variant="top" src={this.props.pImg} width="25" height="240"/>
                         <Card.Body>
-                            <Card.Title >{this.props.pTitle}</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="pltext">{this.props.pTitle}</Card.Title>
+                            <Card.Text className = "pltext">
                                 {this.props.pText}
                             </Card.Text>
-                            <Button variant="primary" href = "https://github.com/ogkama/">Visit project</Button>
+                            <Button variant="outline-secondary"  href = "https://github.com/ogkama/">Visit project</Button>
                         </Card.Body>
                     </Card>
                
